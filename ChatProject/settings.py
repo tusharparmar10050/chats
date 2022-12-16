8305d6dd3ab54591c18602d4bb811fcbf463f929
tusharparmar10050 <tusharparmar10050@gmail.com>
2022-12-16 17:13:11 +0530

    final commit

--- ChatProject/settings.py
+++ ChatProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lj#*@zy5fz!odytcrukkj-vxeyt^x+*9+mvniuavj=+%ie)*_%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -127,3 +127,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles_build', 'static'),)
