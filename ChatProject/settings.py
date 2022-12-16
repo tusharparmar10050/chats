7670be978bbc25d484fbdf739cc5dd8490ceb168
tusharparmar10050 <tusharparmar10050@gmail.com>
2022-12-16 17:31:33 +0530

    setting changes commit

--- ChatProject/settings.py
+++ ChatProject/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,7 +127,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import os
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles_build', 'static'),)
